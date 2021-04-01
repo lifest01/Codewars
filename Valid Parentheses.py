e1bfff6fc85a648549d771fac12ceb9d388a4933
lifest01 <45783329+lifest01@users.noreply.github.com>
2021-04-01 23:09:06 +0300

    Update and rename Valid Parentheses to Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,4 +1,4 @@
-Details:
+'''Details:
 Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
 Examples
@@ -12,6 +12,7 @@ Constraints
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 My solution:
+'''
 def valid_parentheses(text):
     l = len(text)
     c = 0
