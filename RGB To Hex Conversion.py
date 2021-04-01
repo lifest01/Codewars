28cd7c8ffeeb1bcacf52df4f8884c23afefdb0c1
lifest01 <45783329+lifest01@users.noreply.github.com>
2021-04-01 23:08:22 +0300

    Update and rename RGB To Hex Conversion to RGB To Hex Conversion.py

--- RGB To Hex Conversion.py
+++ RGB To Hex Conversion.py
@@ -1,4 +1,4 @@
-Details:
+'''Details:
 The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
 Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
 
@@ -14,6 +14,7 @@ rgb(148, 0, 211) # returns 9400D3
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 My solution:
+'''
   def rgb(r, g, b):
     value = {
         0:'0',
