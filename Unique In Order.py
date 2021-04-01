7fab5a5cc23694f70a67947fe45231d8e8d7c187
lifest01 <45783329+lifest01@users.noreply.github.com>
2021-04-01 23:08:51 +0300

    Update and rename Unique In Order to Unique In Order.py

--- Unique In Order.py
+++ Unique In Order.py
@@ -1,4 +1,4 @@
-Details:
+'''Details:
 Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements 
 with the same value next to each other and preserving the original order of elements.
 
@@ -10,6 +10,7 @@ unique_in_order([1,2,2,3,3])       == [1,2,3]
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 My solution:
+'''
   def unique_in_order(iterable):
     string = []
     for i in range(len(iterable)):
